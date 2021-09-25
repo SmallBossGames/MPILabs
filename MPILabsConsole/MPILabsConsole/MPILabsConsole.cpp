ff7bb8bb7e81952b3e6f940db017c23ed6238854
Luzyanin Alexey <261-96@mail.ru>
2021-09-25 17:21:08 +0700

    working scatterv

--- MPILabsConsole/MPILabsConsole/MPILabsConsole.cpp
+++ MPILabsConsole/MPILabsConsole/MPILabsConsole.cpp
@@ -15,23 +15,10 @@ void CalculateResult(int** matrix, int* vector) {
 
 }
 
-void InitRandomMatrix(double_t** matrix, size_t m, size_t n)
+void InitRandomArray(double_t* vector, size_t m)
 {
     for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            matrix[i][j] = rand() % 100;
-        }
-    }   
-}
-
-void InitRandomVector(double_t* vector, size_t m)
-{
-    for (int i = 0; i < m; i++)
-    {
         vector[i] = rand() % 100;
-    }
 }
 
 int main()
@@ -47,8 +34,6 @@ int main()
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     size_t m = 0, n = 0;
-
-    
 
     if (rank == 0)
     {
@@ -66,56 +51,68 @@ int main()
     double_t* vector = new double[m];
 
     if (rank == 0)
-    {
-        InitRandomVector(vector, m);
-    }
+        InitRandomArray(vector, m);
 
     MPI_Bcast(vector, m, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    /*string sb;
-
-    for (size_t i = 0; i < m; i++)
-    {
-        sb.append(to_string(vector[i])).append(", ");
-    }
-
-    cout << sb << endl;*/
-
+    double_t* matrix = new double[m * n];
 
     if (rank == 0)
     {
         cout << "Threads: " << size << endl;
-
-        double_t** matrix = new double*[m];
-
-        for (size_t i = 0; i < m; i++)
-        {
-            matrix[i] = new double[n];
-        }
-
-        InitRandomMatrix(matrix, m, n);
-        
-
-        for (size_t i = 0; i < m; i++)
-        {
-            delete[] matrix[i];
-        }
-
-        delete[] matrix;
-    } 
-    else
-    {
-        cout << "I'm fucking slave" << endl;
+        InitRandomArray(matrix, m * n);
     }
-
-    
+    else
+        cout << "I'm fucking slave #" << rank << endl;
 
     cout << "Rank: " << rank << endl;
     cout << "M: " << m << endl;
     cout << "N: " << n << endl;
 
+    int* sendNumber;
+    int* sendIndex;
+    double_t* receivedPartialMatrix;
+    int notSendRows = n;
+
+    sendNumber = new int[size];
+    sendIndex = new int[size];
+    receivedPartialMatrix = new double_t[size];
+
+    int rowNumber = n / size;
+    sendNumber[0] = rowNumber * n;
+    sendIndex[0] = 0;
+    receivedPartialMatrix[0] = rowNumber * n;
+
+    for (int i = 1; i < size; i++) {
+        notSendRows -= rowNumber;
+        rowNumber = notSendRows / (size - i);
+        sendNumber[i] = rowNumber * n;
+        sendIndex[i] = sendIndex[i - 1] + sendNumber[i - 1];
+    }
+
+    MPI_Scatterv(matrix, sendNumber, sendIndex, MPI_DOUBLE, receivedPartialMatrix, sendNumber[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    /*for (size_t i = 0; i < m; i++)
+    {
+    }*/
+
+    string sb;
+
+    for (int i = 1; i < rowNumber * n; i++)
+        sb.append(to_string(receivedPartialMatrix[i])).append(", ");
+
+    cout << "Rank #" << rank << " received matrix: " << sb << endl;
+
+    //cout << sb << endl;
+
+    delete[] sendNumber;
+    delete[] sendIndex;
 
     delete[] vector;
+
+    if (rank == 0)
+        delete[] matrix;
+
 
     MPI_Finalize();
 }
