3fbfe7d94deddad384a9a64e0a0c8a258b02fed4
Алексей Гардер <aleksey_garder@outlook.com>
2021-09-25 15:54:09 +0700

    Brodacast vector implemented

--- MPILabsConsole/MPILabsConsole/MPILabsConsole.cpp
+++ MPILabsConsole/MPILabsConsole/MPILabsConsole.cpp
@@ -1,13 +1,12 @@
 
 #include <iostream>
 #include <mpi.h>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-const int n = 10;
-const int m = 10;
-
-void CalculateResult(int matrix[n][m], int vector[m]) {
+void CalculateResult(int** matrix, int* vector) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int size;
@@ -16,25 +15,107 @@ void CalculateResult(int matrix[n][m], int vector[m]) {
 
 }
 
+void InitRandomMatrix(double_t** matrix, size_t m, size_t n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i][j] = rand() % 100;
+        }
+    }   
+}
+
+void InitRandomVector(double_t* vector, size_t m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        vector[i] = rand() % 100;
+    }
+}
+
 int main()
 {
-    int threadsCount;
-    cout << "Enter count of threads" << endl;
-    cin >> threadsCount;
-
-    int matrix[n][m];
-    int vector[m];
-
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            matrix[i][j] = rand() % 100;
-
-    for (int i = 0; i < m; i++)
-        vector[i] = rand() % 100;
-
     MPI_Init(NULL, NULL);
 
-    CalculateResult(matrix, vector);
+    int rank;
+
+    int size;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    size_t m = 0, n = 0;
+
+    
+
+    if (rank == 0)
+    {
+        cout << "Write m:" << endl;
+        cin >> m;
+
+        cout << "Write n:" << endl;
+        cin >> n;
+    }
+
+    MPI_Bcast(&m, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
+
+    MPI_Bcast(&n, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
+
+    double_t* vector = new double[m];
+
+    if (rank == 0)
+    {
+        InitRandomVector(vector, m);
+    }
+
+    MPI_Bcast(vector, m, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    /*string sb;
+
+    for (size_t i = 0; i < m; i++)
+    {
+        sb.append(to_string(vector[i])).append(", ");
+    }
+
+    cout << sb << endl;*/
+
+
+    if (rank == 0)
+    {
+        cout << "Threads: " << size << endl;
+
+        double_t** matrix = new double*[m];
+
+        for (size_t i = 0; i < m; i++)
+        {
+            matrix[i] = new double[n];
+        }
+
+        InitRandomMatrix(matrix, m, n);
+        
+
+        for (size_t i = 0; i < m; i++)
+        {
+            delete[] matrix[i];
+        }
+
+        delete[] matrix;
+    } 
+    else
+    {
+        cout << "I'm fucking slave" << endl;
+    }
+
+    
+
+    cout << "Rank: " << rank << endl;
+    cout << "M: " << m << endl;
+    cout << "N: " << n << endl;
+
+
+    delete[] vector;
 
     MPI_Finalize();
 }
